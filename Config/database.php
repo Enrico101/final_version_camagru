71c98fc6cf05b1035dc8e73f2518c39cb81a7632
Enrico <50211488+Enrico101@users.noreply.github.com>
2019-12-09 09:56:54 +0200

    Update database.php

--- Config/database.php
+++ Config/database.php
@@ -2,5 +2,5 @@
     $DB_DSN = "mysql:host=localhost;db_name=camagru_users";
     $DB_DSN_2 = "mysql:host=localhost";
     $DB_USER = "root";
-    $DB_PASSWORD = "Mark444";
+    $DB_PASSWORD = "password";
 ?>
