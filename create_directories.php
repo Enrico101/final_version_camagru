c10f289c6ecc205a61ccd71bc1a649b28d49226c
Enrico <50211488+Enrico101@users.noreply.github.com>
2020-12-30 20:43:21 +0200

    Update create_directories.php

--- create_directories.php
+++ create_directories.php
@@ -1,10 +1,10 @@
 <?php
     $trash = mkdir("Trash");
     echo "Trash directory created: " . $trash;
-    echo "\n"
+    echo "\n";
     $Uploads = mkdir("Uploads");
     echo "Uploads directory created: " . $Uploads;
-    echo "\n"
+    echo "\n";
     $tmp_uploads = mkdir("tmp_img");
     echo "Tmp_img directory created: " . $tmp_uploads;
     echo "\n";
