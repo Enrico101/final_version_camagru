b10a167b6a3d4fc23182708f6399835ebb3226a3
Enrico <50211488+Enrico101@users.noreply.github.com>
2021-01-07 19:56:17 +0200

    Update setup_db.php

--- Config/setup_db.php
+++ Config/setup_db.php
@@ -12,9 +12,9 @@
 
 
     $q = "CREATE DATABASE IF NOT EXISTS Camagru;";
-    $q_one = "CREATE TABLE IF NOT EXISTS `Camagru`.`Users` ( `User_ID` INT NOT NULL AUTO_INCREMENT , `Username` VARCHAR(255) NOT NULL , `Password` VARCHAR(1000) NOT NULL , `Email` VARCHAR(60) NOT NULL , `Token` VARCHAR(20) NOT NULL , `Status` VARCHAR(20) NOT NULL , `Connection` VARCHAR(20) NOT NULL , `Notifications` VARCHAR(20) NOT NULL , PRIMARY KEY (`User_ID`));";
-    $q_two = "CREATE TABLE IF NOT EXISTS `Camagru`.`Images` ( `Image_ID` INT NOT NULL AUTO_INCREMENT , `User_ID` INT, `Image` VARCHAR(1000) NOT NULL , PRIMARY KEY (`Image_ID`), FOREIGN KEY (`User_ID`) REFERENCES Users(`User_ID`));";
-    $q_three = "CREATE TABLE IF NOT EXISTS `Camagru`.`Likes` ( `User_ID` INT , `Image_ID` INT, FOREIGN KEY (`User_ID`) REFERENCES Users(User_ID), FOREIGN KEY (`Image_ID`)REFERENCES Images(Image_ID));";
+    $q_one = "CREATE TABLE IF NOT EXISTS `Camagru`.`Users` ( `User_ID` INT NOT NULL UNIQUE AUTO_INCREMENT , `Username` VARCHAR(255) NOT NULL , `Password` VARCHAR(1000) NOT NULL , `Email` VARCHAR(60) NOT NULL , `Token` VARCHAR(20) NOT NULL , `Status` VARCHAR(20) NOT NULL , `Connection` VARCHAR(20) NOT NULL , `Notifications` VARCHAR(20) NOT NULL , PRIMARY KEY (`User_ID`));";
+    $q_two = "CREATE TABLE IF NOT EXISTS `Camagru`.`Images` ( `Image_ID` INT NOT NULL UNIQUE AUTO_INCREMENT , `User_ID` INT, `Image` VARCHAR(1000) NOT NULL , PRIMARY KEY (`Image_ID`), FOREIGN KEY (`User_ID`) REFERENCES Users(`User_ID`));";
+    $q_three = "CREATE TABLE IF NOT EXISTS `Camagru`.`Likes` ( `User_ID` INT UNIQUE , `Image_ID` INT, FOREIGN KEY (`User_ID`) REFERENCES Users(User_ID), FOREIGN KEY (`Image_ID`)REFERENCES Images(Image_ID));";
     $q_four = "CREATE TABLE IF NOT EXISTS `Camagru`.`Comments` ( `Comment` VARCHAR(1000) NOT NULL , `User_ID` INT, `Image_ID` INT, FOREIGN KEY (`User_ID`) REFERENCES Users(`User_ID`), FOREIGN KEY (`Image_ID`) REFERENCES Images(`Image_ID`));";
     
     $rs = $db->prepare($q);
